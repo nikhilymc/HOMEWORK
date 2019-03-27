84f067d705e8678b441e75913f414fa444d4b838
nikhilymc <ymckrishna@gmail.com>
2019-03-27 10:41:47 +0530

    funstion multiplication

--- js/function.js
+++ js/function.js
@@ -2,8 +2,10 @@ var num1=100;
 var num2=160;
 var num=add(num1,num2);
 var num3=sub(num1,num2);
+mul(num1,num2);
 console.log(z);
 console.log(a);
+console.log(b);
 
 function add(x,y){
     z=x+y;
@@ -13,4 +15,9 @@ function sub(x,y){
     a=x-y;
     return z;
 
-}                                                       
+}   
+function mul(x,y){
+    b=x*y;
+    return b;
+
+}                                                                                                           
