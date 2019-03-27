14c5c2a9806d425b9a39cdfbe5bce6b2d913e3c1
nikhilymc <ymckrishna@gmail.com>
2019-03-27 10:37:49 +0530

    funstion

---
+++ js/function.js
@@ -0,0 +1,16 @@
+var num1=100;
+var num2=160;
+var num=add(num1,num2);
+var num3=sub(num1,num2);
+console.log(z);
+console.log(a);
+
+function add(x,y){
+    z=x+y;
+    return z;
+}
+function sub(x,y){
+    a=x-y;
+    return z;
+
+}                                                       
